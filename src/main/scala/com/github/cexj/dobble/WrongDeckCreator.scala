4dae23597d78d1e00993c3cef088ef242b4992c5
Andrea Cesaro <ace@n-side.com>
2021-11-29 23:41:38 +0100

    clean up

--- src/main/scala/com/github/cexj/dobble/WrongDeckCreator.scala
+++ src/main/scala/com/github/cexj/dobble/WrongDeckCreator.scala
@@ -1,36 +1,26 @@
 package com.github.cexj.dobble
 
+import scala.annotation.tailrec
 import scala.collection.SortedMap
 
-object WrongDeckCreator extends App {
-
-  val n = 4
-  val x = create(n*n-n+1 , n)
-  println(x)
-  println(x.size)
-
-
+object WrongDeckCreator {
 
   def create(symbols: Int, cardSize: Int): Seq[Seq[Int]] = {
+    @tailrec
     def createTR(symbol: Int, deck: Seq[Seq[Int]], partialCards: SortedMap[Int, Seq[Seq[Int]]]): Seq[Seq[Int]] = {
 
       if(symbol > symbols) {
         deck
       } else {
-        val newMapCandidates = SortedMap.from((0 to cardSize).map(i => i -> (partialCards.getOrElse(i-1, List()).map(symbol +: _) ++ partialCards.get(i).getOrElse(List()))))
-        println("newMapCandidates: " + newMapCandidates)
-        val candidates = newMapCandidates.get(cardSize).getOrElse(List())
-        println("candidates: " + candidates)
+        val newMapCandidates = SortedMap.from((0 to cardSize).map(i => i -> (partialCards.getOrElse(i-1, Seq.empty).map(symbol +: _) ++ partialCards.getOrElse(i, Seq.empty))))
+        val candidates = newMapCandidates.getOrElse(cardSize, Seq.empty)
         val partialCandidates = newMapCandidates.dropRight(1)
-        println("partialCandidates: " + partialCandidates)
         val newDeck = addToDeck(deck, candidates)
-        println("newDeck: " + newDeck)
         val newPartialCards = refinePartialCards(partialCandidates, deck, symbols - symbol, cardSize)
-        println("newPartialCards: " + newPartialCards)
         createTR(symbol + 1, newDeck, newPartialCards)
       }
     }
-    createTR(1, Seq(), SortedMap(0 -> Seq(Seq())))
+    createTR(1, Seq.empty, SortedMap(0 -> Seq(Seq.empty)))
   }
 
   def addToDeck(deck: Seq[Seq[Int]], candidates: Seq[Seq[Int]] ): Seq[Seq[Int]] = {
@@ -53,11 +43,11 @@ object WrongDeckCreator extends App {
   }
 
   def globalCheck[Int](deck: Seq[Seq[Int]], candidate: Seq[Int]): Boolean = {
-    deck.map(card => card.intersect(candidate).size).filter(_ != 1).isEmpty
+    deck.map(card => card.intersect(candidate).size).exists(_ != 1)
   }
 
   def localCheck(deck: Seq[Seq[Int]], candidate: Seq[Int]): Boolean = {
-    deck.map(card => check(card, candidate)).filter(_ > 1).isEmpty
+    deck.map(card => check(card, candidate)).exists(_ > 1)
   }
 
 }
